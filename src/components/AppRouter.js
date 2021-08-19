180eab91cf18cbc430886b6b614a857347e3d083
Arthur <artur17harutyunyan@gmail.com>
2021-08-19 10:58:22 +0400

    changed input and button functionality

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -4,36 +4,26 @@ import { privateRoutes, publicRoutes } from '../routes';
 import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/routeConsts';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import Context from '../context/Context';
-// import Loader from '../loader/Loader';
 
 const AppRouter = () => {
     const { auth } = useContext(Context);
-    const [ user ] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-    return user ?
-        (
-            <Switch>
-                {
-                    privateRoutes.map(({ path, Component }, idx) =>
-                        <Route path={path} component={Component} exact={true}
-                               key={idx} />
-                    )
-                }
-                <Redirect to={CHAT_ROUTE} />
-            </Switch>
-        )
-        :
-        (
-            <Switch>
-                {
-                    publicRoutes.map(({ path, Component }, idx) =>
-                        <Route path={path} component={Component} exact={true}
-                               key={idx} />
-                    )
-                }
-                <Redirect to={LOGIN_ROUTE} />
-            </Switch>
-        );
+    return user ? (
+        <Switch>
+            {privateRoutes.map(({ path, Component }) => (
+                <Route path={path} component={Component} exact={true} key={path} />
+            ))}
+            <Redirect to={CHAT_ROUTE} />
+        </Switch>
+    ) : (
+        <Switch>
+            {publicRoutes.map(({ path, Component }) => (
+                <Route path={path} component={Component} exact={true} key={path} />
+            ))}
+            <Redirect to={LOGIN_ROUTE} />
+        </Switch>
+    );
 };
 
 export default AppRouter;
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -9,9 +9,9 @@ import firebase from 'firebase';
 const Chat = () => {
     const inpref = useRef();
     const { auth, firestore } = useContext(Context);
-    const [ user ] = useAuthState(auth);
-    const [ inpVal, setInpVal ] = useState('');
-    const [ messages, loading ] = useCollectionData(
+    const [user] = useAuthState(auth);
+    const [inpVal, setInpVal] = useState('');
+    const [messages, loading] = useCollectionData(
         firestore.collection('messages').orderBy('createdAt')
     );
 
@@ -21,7 +21,7 @@ const Chat = () => {
                 uid: user.uid,
                 displayName: user.displayName,
                 text: inpVal,
-                createdAt: firebase.firestore.FieldValue.serverTimestamp()
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             });
         }
         setInpVal('');
@@ -29,62 +29,60 @@ const Chat = () => {
     };
 
     const sendMessageByKey = (event) => {
-        if (event.key === 'Enter') {
-            if (inpVal.trim()) {
-                firestore.collection('messages').add({
-                    uid: user.uid,
-                    displayName: user.displayName,
-                    text: inpVal,
-                    createdAt: firebase.firestore.FieldValue.serverTimestamp()
-                });
-            }
+        if (event.key === 'Enter' && inpVal.trim()) {
+            firestore.collection('messages').add({
+                uid: user.uid,
+                displayName: user.displayName,
+                text: inpVal,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            });
             setInpVal('');
             inpref.current.focus();
         }
-    }
+    };
 
     if (loading) return <Loader />;
 
     return (
-        <Container className='p-0'>
-            <h2 className='userName p-0'>{user?.displayName}</h2>
-            <div className='container justify-content-center mt-5 p-0'
-                 style={{ height: window.innerHeight - 58 }}
+        <Container className="p-0">
+            <h2 className="userName p-0">{user?.displayName}</h2>
+            <div
+                className="container justify-content-center mt-5 p-0"
+                style={{ height: window.innerHeight - 58 }}
             >
-                <div className='chatBlock border border-secondary p-3'>
-                    {messages.map((message, idx) =>
-                        <Card style={{
-                            width: 'fit-content',
-                            border: user?.uid === message.uid ? '3px solid red' : '3px solid blue',
-                            marginLeft: user?.uid === message.uid ? 'auto' : 0
-                        }}
-                              key={idx}
-                              className='p-1 mb-2 '>
-                            <Card.Body className='p-0 m-0'>
-                                <Card.Title>
-                                    {message.displayName}
-                                </Card.Title>
-                                <Card.Text>
-                                    {message.text}
-                                </Card.Text>
+                <div className="chatBlock border border-secondary p-3">
+                    {messages.map((message) => (
+                        <Card
+                            style={{
+                                width: 'fit-content',
+                                border:
+                                    user?.uid === message.uid
+                                        ? '3px solid red'
+                                        : '3px solid blue',
+                                marginLeft: user?.uid === message.uid ? 'auto' : 0,
+                            }}
+                            key={message.uid}
+                            className="p-1 mb-2 "
+                        >
+                            <Card.Body className="p-0 m-0">
+                                <Card.Title>{message.displayName}</Card.Title>
+                                <Card.Text>{message.text}</Card.Text>
                             </Card.Body>
                         </Card>
-                    )}
+                    ))}
                 </div>
-                <div className='d-flex mt-3'>
-                    <Form.Control type='text' value={inpVal}
-                                  onChange={e => setInpVal(e.target.value)}
-                                  className='me-3 border-secondary'
-                                  placeholder='Type your message...'
-                                  onKeyPress={sendMessageByKey}
-                                  ref={inpref}
+                <div className="d-flex mt-3">
+                    <Form.Control
+                        type="text"
+                        value={inpVal}
+                        onChange={(e) => setInpVal(e.target.value)}
+                        className="me-3 border-secondary"
+                        placeholder="Type your message..."
+                        onKeyPress={sendMessageByKey}
+                        ref={inpref}
                     />
-                    <Button
-                        onClick={sendMessage}
-                        className='flex-grow-1 w-25'
-                    >
+                    <Button onClick={sendMessage} className="flex-grow-1 w-25">
                         Send
-
                     </Button>
                 </div>
             </div>
